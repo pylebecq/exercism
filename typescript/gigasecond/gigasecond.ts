44d25d4fa6ae73f1525198d1838d77dfa2b978c5
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-04-28 12:15:18 +0200

    [typescript][gigasecond] Introduce constant to remove magic number

--- typescript/gigasecond/gigasecond.ts
+++ typescript/gigasecond/gigasecond.ts
@@ -1,7 +1,9 @@
+const GIGASECOND_IN_MILLISECONDS = 1e12;
+
 export default class Gigasecond {
     constructor(private readonly origin: Date) {}
 
     date(): Date {
-        return new Date(this.origin.getTime() + 1e12);
+        return new Date(this.origin.getTime() + GIGASECOND_IN_MILLISECONDS);
     }
 }
