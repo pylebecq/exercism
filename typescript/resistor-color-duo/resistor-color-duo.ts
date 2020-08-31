65ef68506962ed7e49dcd1bc3f02a360f6463ef0
Pierre-Yves Lebecq <pierre-yves@zenaton.com>
2020-08-31 09:30:25 +0200

    [typescript][resistor-color-duo] Apply suggestions from mentor

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,37 +1,34 @@
-const colorValues = {
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9,
-};
-
-type Color = keyof typeof colorValues;
-
-function isValidColor(color: string): color is Color {
-  return color in colorValues;
-}
-
-function isValidColorList(colors: string[]): colors is Array<Color> {
-  return colors.length >= 2 && colors.every(isValidColor);
-}
-
 export class ResistorColor {
+  static ColorValues = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+
   private colors: Color[];
 
-  constructor(colors: string[]) {
-    if (!isValidColorList(colors)) {
+  constructor(colors: Color[]) {
+    if (colors.length < 2) {
       throw new Error("At least two colors need to be present");
     }
 
-    this.colors = colors;
+    this.colors = colors.slice(0, 2);
   }
 
-  value = (): number =>
-    10 * colorValues[this.colors[0]] + colorValues[this.colors[1]];
+  value(): number {
+    const [first, second] = this.colors;
+
+    return (
+      10 * ResistorColor.ColorValues[first] + ResistorColor.ColorValues[second]
+    );
+  }
 }
+
+type Color = keyof typeof ResistorColor.ColorValues;
